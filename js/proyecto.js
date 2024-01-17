6c500718b44bb05057f77730128d550c4b0d35b7
Arevi9 <148113389+Arevi9@users.noreply.github.com>
2024-01-17 21:04:32 +0100

    d

--- js/proyecto.js
+++ js/proyecto.js
@@ -98,3 +98,30 @@ function mostrar(objetos){
 
 
 
+ // Función para mostrar u ocultar el botón basado en la posición de desplazamiento y el ancho de la ventana
+ window.onscroll = function() {
+  mostrarOcultarBoton();
+};
+
+window.onresize = function() {
+  mostrarOcultarBoton();
+};
+
+function mostrarOcultarBoton() {
+  var botonSubir = document.getElementById("botonSubir");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (window.innerWidth < 768) {
+      botonSubir.style.display = "block";
+    } else {
+      botonSubir.style.display = "none";
+    }
+  } else {
+    botonSubir.style.display = "none";
+  }
+}
+
+// Función para desplazarse hacia arriba al hacer clic en el botón
+function subirAlInicio() {
+  document.body.scrollTop = 0;  // Para navegadores que no soportan document.documentElement
+  document.documentElement.scrollTop = 0;  // Funciona en la mayoría de los navegadores
+}
